f11380bef8efa59f30acda469291ea412a8b9bb3
laura-l77 <lauralebreton77@gmail.com>
2025-05-15 11:04:09 +0200

    Update reservation_salle.php

--- PHP/reservation_salle.php
+++ PHP/reservation_salle.php
@@ -2,23 +2,28 @@
 <html lang="fr">
 <head>
     <meta charset="UTF-8">
-    <title>Réservation Salle</title>
+    <title>Réservation Salle VR</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    
+    <!-- Feuilles de style -->
+    <link rel="stylesheet" href="../CSS/style.css">
+    <link rel="stylesheet" href="../CSS/salles.css">
+    <link rel="stylesheet" href="../CSS/header.css">
 
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <meta http-equiv="X-UA-Compatible" content="ie=edge" />
-    <link rel="stylesheet" href="../CSS/style.css" />
-    <link rel="stylesheet" href="../CSS/salles.css" />
-    <link rel="stylesheet" href="../CSS/header.css" />
+    <!-- Bibliothèque A-Frame pour la VR -->
     <script src="https://aframe.io/releases/1.7.0/aframe.min.js"></script>
-    <title>Réservation VR</title>
 </head>
 <body>
-    <h1>Réservation </h1>
 
-	<?php
-    include("header.php");
-    include("aside.php");
+    <!-- En-tête de la page -->
+    <h1>Réservation</h1>
+
+    <!-- Inclusion des fichiers PHP pour le header et l'aside -->
+    <?php 
+        include("header.php");
+        include("aside.php");
     ?>
+
 </body>
 </html>

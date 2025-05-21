399fd727213b7062d71c298c12173e235f216789
goola971 <janviercharly97114@gmail.com>
2025-05-21 16:28:46 +0200

    save

--- PHPpure/connexion.php
+++ PHPpure/connexion.php
@@ -8,6 +8,9 @@ $dbname = "sae201";
 
 $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $user, $pass);
 
+$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+
 
 function getUserRole($idUser, $pdo)
 {

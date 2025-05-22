26602c1e85a92543d063d8d618e8fc7073b48d2f
soya ❀ <smgbadagni@gmail.com>
2025-05-22 16:23:23 +0200

    savee

--- PHP/inscription.php
+++ PHP/inscription.php
@@ -1,14 +1,13 @@
 <!DOCTYPE html>
-<html lang="fr">
+<html lang="en">
 <head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <meta http-equiv="X-UA-Compatible" content="ie=edge" />
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/css/bootstrap.min.css" rel="stylesheet">
-    <link rel="stylesheet" href="../CSS/style.css" />
-    <link rel="stylesheet" href="../CSS/TEST.css" />
-    <link rel="stylesheet" href="../CSS/header.css" />
-    <title>TEST Multi-step Form</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4Q6Gf2aSP4eDXB8Miphtr37CMZZQ5oXLH2yaXMJ2w8e2ZtHTl7GptT4jmndRuHDT" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="inscription.css">
+    <link rel="stylesheet" href="header.css">
+    <title>Inscription</title>
 </head>
 <body>
     <main>
@@ -73,6 +72,6 @@
             </div>
         </section>
     </main>
-    <script src="../JS/TEST.js"></script>
+    <script src="js/inscription.js"></script>
 </body>
 </html> 

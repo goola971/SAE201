e017c7245c6bf8580da5097ecfec66cc23be2bee
goola971 <janviercharly97114@gmail.com>
2025-06-05 15:53:02 +0200

    saave

--- JS/calendar.js
+++ JS/calendar.js
@@ -29,7 +29,7 @@ document.addEventListener("DOMContentLoaded", function () {
 			center: "title",
 			right: "dayGridMonth,dayGridWeek,dayGridDay"
 		},
-		events: "http://localhost/SAE201/PHPpure/get_reservations.php",
+		events: "../PHPpure/get_reservations.php",
 
 		eventDidMount: function (info) {
 			const container = info.el.querySelector(".fc-event-main-frame");
